af438b5814454ffe375b4a25b0282bfd5f360577
AleMidolo <alemidolo@gmail.com>
2025-02-23 15:16:13 +0100

    python italian iter_0

---
+++ analyze/62ece4992e6aefcf4aabbd85.py
@@ -0,0 +1,18 @@
+def find_roots(graph, rdflib.RDFS.subClassOf):
+    """
+    restituirà un insieme contenente tutte le radici della gerarchia delle sottoclassi.
+
+    Presuppone triple nella forma (figlio, prop, genitore), ovvero la direzione di
+    `RDFS.subClassOf` o `SKOS.broader`.
+    """
+    roots = set()
+    subclasses = set()
+
+    for child, _, parent in graph.triples((None, rdflib.RDFS.subClassOf, None)):
+        subclasses.add(child)
+
+    for child in subclasses:
+        if child not in subclasses:
+            roots.add(child)
+
+    return roots
