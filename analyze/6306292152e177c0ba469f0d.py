1b43c8b90a76536ee60fee0d2f176101e995d9ae
AleMidolo <alemidolo@gmail.com>
2025-02-23 15:14:55 +0100

    removed

--- analyze/6306292152e177c0ba469f0d.py
+++
@@ -1,5 +0,0 @@
-def identify_request(request: RequestType) -> bool:
-    """
-    Prova a identificare se si tratta di una richiesta Matrix.
-    """
-    return isinstance(request, MatrixRequestType)
--- analyze/6306292252e177c0ba469f11.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def format_dt(dt):
-    """
-    Formatta un oggetto datetime nel modo in cui i nodi D* si aspettano.
-    """
-    return dt.strftime('%Y-%m-%dT%H:%M:%S')
