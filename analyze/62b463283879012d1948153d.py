11037fae6b56c36242abdb16e6a62696174b427c
AleMidolo <alemidolo@gmail.com>
2025-02-23 15:11:21 +0100

    Removed

--- analyze/62b463283879012d1948153d.py
+++
@@ -1,9 +0,0 @@
-def match_pubdate(node, pubdate_xpaths):
-    """
-    Restituisce la prima corrispondenza nella lista `pubdate_xpaths`.
-    """
-    for xpath in pubdate_xpaths:
-        result = node.xpath(xpath)
-        if result:
-            return result[0]
-    return None
--- analyze/62ece4982e6aefcf4aabbd70.py
+++
@@ -1,9 +0,0 @@
-def os_is_mac():
-    """
-    Verifica se il sistema operativo è macOS
-
-    :return: True se il sistema operativo è macOS
-    :rtype: bool
-    """
-    import platform
-    return platform.system() == "Darwin"
