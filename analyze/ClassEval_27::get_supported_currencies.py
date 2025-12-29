7fcc4b67939d73379d04b08be184fbcfc3ff912f
Alessandro Midolo <alemidolo@gmail.com>
2025-12-29 10:25:59 +0100

    Cleaned

--- analyze/ClassEval_27::get_supported_currencies.py
+++
@@ -1,9 +0,0 @@
-def get_supported_currencies(self):
-        """
-        समर्थित मुद्रा प्रकारों की सूची लौटाता है
-        :return:list, सभी समर्थित मुद्रा प्रकार
-        >>> cc = CurrencyConverter()
-        >>> cc.get_supported_currencies()
-        ['USD','EUR','GBP','JPY','CAD','AUD','CNY']
-        """
-        return list(self.rates.keys())
--- analyze/ClassEval_85::get_mode.py
+++
@@ -1,6 +0,0 @@
-def get_mode(self):
-        """
-        वर्तमान कार्य मोड प्राप्त करें
-        :return mode: str, कार्य मोड। केवल ['heat', 'cool']
-        """
-        return self.mode
